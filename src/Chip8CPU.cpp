63e49f302ccf08711c1dd26719bf2ce2b940bbda
LuminousFondue <kseldom@proton.me>
2025-08-18 18:21:54 -0400

    Added Comments for each opcode

--- src/Chip8CPU.cpp
+++ src/Chip8CPU.cpp
@@ -173,204 +173,414 @@ void Chip8CPU::handle_FXXX(uint16_t opcode)
     }
 }
 
+/**
+ * CLS - Clear the Display
+ */
 void Chip8CPU::opcode_00E0(uint16_t opcode)
 {
     /* TODO: implement 00E0 (CLS) */
     spdlog::debug("Opcode: 00E0");
 }
 
+/**
+ * 00EE: RET - Return from a subroutine
+ *
+ * The interpreter sets the program counter to the
+ * address at the top of the stack,
+ * then subtracts 1 from the stack pointer.
+ */
 void Chip8CPU::opcode_00EE(uint16_t opcode)
 {
     /* TODO: implement 00EE (RET) */
     spdlog::debug("Opcode: 00EE");
 }
 
+/**
+ * 1NNN: JP addr - Jump to location nnn
+ *
+ * The interpreter sets the program counter to nnn.
+ */
 void Chip8CPU::opcode_1NNN(uint16_t opcode)
 {
     /* TODO: implement 1NNN (JP addr) */
     spdlog::debug("Opcode: 1NNN");
 }
 
+/**
+ * 2NNN: CALL addr - Call subroutine at nnn
+ *
+ * The interpreter increments the stack pointer,
+ * then puts the current PC on the top of the stack.
+ * The PC is then set to nnn.
+ */
 void Chip8CPU::opcode_2NNN(uint16_t opcode)
 {
     /* TODO: implement 2NNN (CALL addr) */
     spdlog::debug("Opcode: 2NNN");
 }
 
+/**
+ * 3XKK: SE Vx, byte - Skip next instruction if Vx == kk
+ *
+ * The interpreter compares register Vx to kk, and if they
+ * are equal, increments the program counter by 2.
+ */
 void Chip8CPU::opcode_3XKK(uint16_t opcode)
 {
     /* TODO: implement 3XKK (SE Vx, byte) */
     spdlog::debug("Opcode: 3XKK");
 }
 
+/**
+ * 4XKK: SNE Vx, byte - Skip next instruction if Vx != kk
+ *
+ * The interpreter compares register Vx to kk, and if they
+ * are not equal, increments the program counter by 2.
+ */
 void Chip8CPU::opcode_4XKK(uint16_t opcode)
 {
     /* TODO: implement 4XKK (SNE Vx, byte) */
     spdlog::debug("Opcode: 4XKK");
 }
 
+/**
+ * 5XY0: SE Vx, Vy - Skip next instruction if Vx == Vy
+ *
+ * The interpreter compares register Vx to register Vy,
+ * and if they are equal, increments the program counter by 2.
+ */
 void Chip8CPU::opcode_5XY0(uint16_t opcode)
 {
     /* TODO: implement 5XY0 (SE Vx, Vy) */
     spdlog::debug("Opcode: 5XY0");
 }
 
+/**
+ * 6XKK: LD Vx, byte - Set Vx = kk
+ *
+ * The interpreter loads the value kk into register Vx.
+ */
 void Chip8CPU::opcode_6XKK(uint16_t opcode)
 {
     /* TODO: implement 6XKK (LD Vx, byte) */
     spdlog::debug("Opcode: 6XKK");
 }
 
+/**
+ * 7XKK: ADD Vx, byte - Set Vx = Vx + kk
+ *
+ * Adds the value kk to the value of register Vx,
+ * then stores the result in Vx.
+ */
 void Chip8CPU::opcode_7XKK(uint16_t opcode)
 {
     /* TODO: implement 7XKK (ADD Vx, byte) */
     spdlog::debug("Opcode: 7XKK");
 }
 
+/**
+ * 8XY0: LD Vx, Vy - Set Vx = Vy
+ *
+ * Stores the value of register Vy in register Vx.
+ */
 void Chip8CPU::opcode_8XY0(uint16_t opcode)
 {
     /* TODO: implement 8XY0 (LD Vx, Vy) */
     spdlog::debug("Opcode: 8XY0");
 }
 
+/**
+ * 8XY1: OR Vx, Vy - Set Vx = Vx OR Vy
+ *
+ * Performs a bitwise OR on the values of Vx and Vy,
+ * then stores the result in Vx. A bitwise OR compares the
+ * corresponding bits from two values, and if either bit is 1,
+ * then the same bit in the result is also 1. Otherwise, it is 0.
+ */
 void Chip8CPU::opcode_8XY1(uint16_t opcode)
 {
     /* TODO: implement 8XY1 (OR Vx, Vy) */
     spdlog::debug("Opcode: 8XY1");
 }
 
+/**
+ * 8XY2: AND Vx, Vy - Set Vx = Vx AND Vy
+ *
+ * Performs a bitwise AND on the values of Vx and Vy,
+ * then stores the result in Vx. A bitwise AND compares the
+ * corresponding bits from two values, and if both bits are 1,
+ * then the same bit in the result is also 1. Otherwise, it is 0.
+ */
 void Chip8CPU::opcode_8XY2(uint16_t opcode)
 {
     /* TODO: implement 8XY2 (AND Vx, Vy) */
     spdlog::debug("Opcode: 8XY2");
 }
 
+/**
+ * 8XY3: XOR Vx, Vy - Set Vx = Vx XOR Vy
+ *
+ * Performs a bitwise XOR on the values of Vx and Vy,
+ * then stores the result in Vx. A bitwise XOR compares the
+ * corresponding bits from two values, and if the bits are different,
+ * then the same bit in the result is 1. Otherwise, it is 0.
+ */
 void Chip8CPU::opcode_8XY3(uint16_t opcode)
 {
     /* TODO: implement 8XY3 (XOR Vx, Vy) */
     spdlog::debug("Opcode: 8XY3");
 }
 
+/**
+ * 8XY4: ADD Vx, Vy - Set Vx = Vx + Vy
+ *
+ * The values of Vx and Vy are added together.
+ * If the result is greater than 8 bits (i.e., > 255,)
+ * VF is set to 1, otherwise 0. Only the lowest 8 bits
+ * of the result are kept, and stored in Vx.
+ */
 void Chip8CPU::opcode_8XY4(uint16_t opcode)
 {
     /* TODO: implement 8XY4 (ADD Vx, Vy) */
     spdlog::debug("Opcode: 8XY4");
 }
 
+/**
+ * 8XY5: SUB Vx, Vy - Set Vx = Vx - Vy
+ *
+ * If Vx > Vy, then VF is set to 1, otherwise 0.
+ * Then Vy is subtracted from Vx, and the results stored in Vx.
+ */
 void Chip8CPU::opcode_8XY5(uint16_t opcode)
 {
     /* TODO: implement 8XY5 (SUB Vx, Vy) */
     spdlog::debug("Opcode: 8XY5");
 }
 
+/**
+ * 8XY6: SHR Vx {, Vy} - Set Vx = Vx SHR 1
+ *
+ * If the least-significant bit of Vx is 1,
+ * then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+ */
 void Chip8CPU::opcode_8XY6(uint16_t opcode)
 {
     /* TODO: implement 8XY6 (SHR Vx {, Vy}) */
     spdlog::debug("Opcode: 8XY6");
 }
 
+/**
+ * 8XY7: SUBN Vx, Vy - Set Vx = Vy - Vx
+ *
+ * If Vy > Vx, then VF is set to 1, otherwise 0.
+ * Then Vx is subtracted from Vy, and the results stored in Vx.
+ */
 void Chip8CPU::opcode_8XY7(uint16_t opcode)
 {
     /* TODO: implement 8XY7 (SUBN Vx, Vy) */
     spdlog::debug("Opcode: 8XY7");
 }
 
+/**
+ * 8XYE: SHL Vx {, Vy} - Set Vx = Vx SHL 1
+ *
+ * If the most-significant bit of Vx is 1,
+ * then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+ */
 void Chip8CPU::opcode_8XYE(uint16_t opcode)
 {
     /* TODO: implement 8XYE (SHL Vx {, Vy}) */
     spdlog::debug("Opcode: 8XYE");
 }
 
+/**
+ * 9XY0: SNE Vx, Vy - Skip next instruction if Vx != Vy
+ *
+ * The values of Vx and Vy are compared, and if they are not equal,
+ * the program counter is increased by 2.
+ */
 void Chip8CPU::opcode_9XY0(uint16_t opcode)
 {
     /* TODO: implement 9XY0 (SNE Vx, Vy) */
     spdlog::debug("Opcode: 9XY0");
 }
 
+/**
+ * ANNN: LD I, addr - Set I = addr
+ *
+ * The value of register I is set to nnn.
+ */
 void Chip8CPU::opcode_ANNN(uint16_t opcode)
 {
     /* TODO: implement ANNN (LD I, addr) */
     spdlog::debug("Opcode: ANNN");
 }
 
+/**
+ * BNNN: JP V0, addr - Jump to location nnn + V0
+ *
+ * The program counter is set to nnn plus the value of V0.
+ */
 void Chip8CPU::opcode_BNNN(uint16_t opcode)
 {
     /* TODO: implement BNNN (JP V0, addr) */
     spdlog::debug("Opcode: BNNN");
 }
 
+/**
+ * CXKK: RND Vx, byte - Set Vx = random byte AND KK
+ *
+ * The interpreter generates a random number from 0 to 255,
+ * which is then ANDed with the value kk. The results are stored in Vx.
+ * See instruction 8xy2 for more information on AND.
+ */
 void Chip8CPU::opcode_CXKK(uint16_t opcode)
 {
     /* TODO: implement CXKK (RND Vx, byte) */
     spdlog::debug("Opcode: CXKK");
 }
 
+/**
+ * DXYN: DRW Vx, Vy, nibble - Draw sprite at (Vx, Vy)
+ *
+ * The interpreter reads n bytes from memory, starting at
+ * the address stored in I. These bytes are then displayed as
+ * sprites on screen at coordinates (Vx, Vy). Sprites are XORed
+ * onto the existing screen. If this causes any pixels to be erased,
+ * VF is set to 1, otherwise it is set to 0. If the sprite is positioned
+ * so part of it is outside the coordinates of the display, it wraps around
+ * to the opposite side of the screen. See instruction 8xy3 for more information
+ * on XOR, and section 2.4, Display, for more information on the Chip-8 screen and sprites.
+ */
 void Chip8CPU::opcode_DXYN(uint16_t opcode)
 {
     /* TODO: implement DXYN (DRW Vx, Vy, nibble) */
     spdlog::debug("Opcode: DXYN");
 }
 
-void Chip8CPU::opcode_EXA1(uint16_t opcode)
-{
-    /* TODO: implement EXA1 (SKNP Vx) */
-    spdlog::debug("Opcode: EXA1");
-}
-
+/**
+ * EX9E: SKP Vx - Skip next instruction if key with value Vx is pressed
+ *
+ * Checks the keyboard, and if the key corresponding to the value of
+ * Vx is currently in the down position, PC is increased by 2.
+ */
 void Chip8CPU::opcode_EX9E(uint16_t opcode)
 {
     /* TODO: implement EX9E (SKP Vx) */
     spdlog::debug("Opcode: EX9E");
 }
 
+/**
+ * EXA1: SKNP Vx - Skip next instruction if key with value Vx is not pressed
+ *
+ * Checks the keyboard, and if the key corresponding to the value of
+ * Vx is currently not in the down position, PC is increased by 2.
+ */
+void Chip8CPU::opcode_EXA1(uint16_t opcode)
+{
+    /* TODO: implement EXA1 (SKNP Vx) */
+    spdlog::debug("Opcode: EXA1");
+}
+
+/**
+ * FX07: LD Vx, DT - Set Vx = delay timer value
+ *
+ * The value of the delay timer is copied into register Vx.
+ */
 void Chip8CPU::opcode_FX07(uint16_t opcode)
 {
     /* TODO: implement FX07 (LD Vx, DT) */
     spdlog::debug("Opcode: FX07");
 }
 
+/**
+ * FX0A: LD Vx, K - Wait for a key press, store the value of the key in Vx
+ *
+ * The interpreter waits for a key press, and when a key is pressed,
+ * the value of the key is stored in register Vx.
+ */
 void Chip8CPU::opcode_FX0A(uint16_t opcode)
 {
     /* TODO: implement FX0A (LD Vx, K) */
     spdlog::debug("Opcode: FX0A");
 }
 
+/**
+ * FX15: LD DT, Vx - Set delay timer = Vx
+ *
+ * The value of register Vx is copied into the delay timer.
+ */
 void Chip8CPU::opcode_FX15(uint16_t opcode)
 {
     /* TODO: implement FX15 (LD DT, Vx) */
     spdlog::debug("Opcode: FX15");
 }
 
+/**
+ * FX18: LD ST, Vx - Set sound timer = Vx
+ *
+ * The value of register Vx is copied into the sound timer.
+ */
 void Chip8CPU::opcode_FX18(uint16_t opcode)
 {
     /* TODO: implement FX18 (LD ST, Vx) */
     spdlog::debug("Opcode: FX18");
 }
 
+/**
+ * FX1E: ADD I, Vx - Add Vx to I
+ *
+ * The value of register Vx is added to the value of register I.
+ */
 void Chip8CPU::opcode_FX1E(uint16_t opcode)
 {
     /* TODO: implement FX1E (ADD I, Vx) */
     spdlog::debug("Opcode: FX1E");
 }
 
+/**
+ * FX29: LD F, Vx - Load font sprite for digit Vx into I
+ *
+ * The value of register Vx is used as an index into the font sprite
+ * data stored in memory. The address of the sprite is loaded into
+ * register I.
+ */
 void Chip8CPU::opcode_FX29(uint16_t opcode)
 {
     /* TODO: implement FX29 (LD F, Vx) */
     spdlog::debug("Opcode: FX29");
 }
 
+/**
+ * FX33: LD B, Vx - Store BCD representation of Vx in memory
+ *
+ * The interpreter takes the value of register Vx, converts it to Binary-Coded Decimal (BCD),
+ * and stores the hundreds digit at address I, the tens digit at I+1, and the ones digit at I+2.
+ */
 void Chip8CPU::opcode_FX33(uint16_t opcode)
 {
     /* TODO: implement FX33 (LD B, Vx) */
     spdlog::debug("Opcode: FX33");
 }
 
+/**
+ * FX55: LD [I], Vx - Store registers V0 through Vx in memory
+ *
+ * The interpreter copies the values of registers V0 through Vx into
+ * memory, starting at the address stored in I.
+ */
 void Chip8CPU::opcode_FX55(uint16_t opcode)
 {
     /* TODO: implement FX55 (LD [I], Vx) */
     spdlog::debug("Opcode: FX55");
 }
 
+/**
+ * FX65: LD Vx, [I] - Read registers V0 through Vx from memory
+ *
+ * The interpreter copies the values from memory, starting at the address
+ * stored in I, into registers V0 through Vx.
+ */
 void Chip8CPU::opcode_FX65(uint16_t opcode)
 {
     /* TODO: implement FX65 (LD Vx, [I]) */
