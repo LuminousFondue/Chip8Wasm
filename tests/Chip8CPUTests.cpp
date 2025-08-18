6b243744f29e9f8d4cf783c5e135cc59cd98ae95
LuminousFondue <kseldom@proton.me>
2025-08-18 19:04:58 -0400

    Added placeholder tests for each opcode method.

--- tests/Chip8CPUTests.cpp
+++ tests/Chip8CPUTests.cpp
@@ -29,7 +29,7 @@ TEST_F(Chip8CPUTest, opcode_00E0_test)
 {
     // Turn on one pixel
     graphics.setPixel(0, 0, 1);
-    EXPECT_EQ(graphics.getPixel(0, 0), 1);
+    EXPECT_EQ(graphics.getPixel(0, 0), 1) << "Pixel should be on";
 
     // Write the opcode for clearing the screen
     memory.write(0x200, 0x00);
@@ -37,5 +37,170 @@ TEST_F(Chip8CPUTest, opcode_00E0_test)
 
     // Execute the cycle
     cpu.cycle();
-    EXPECT_EQ(graphics.getPixel(0, 0), 0);
+    EXPECT_EQ(graphics.getPixel(0, 0), 0) << "Pixel should be off";
+}
+
+TEST_F(Chip8CPUTest, opcode_00EE_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_1NNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_2NNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_3XNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_4XNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_5XY0_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_6XNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_7XNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY0_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY1_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY2_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY3_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY4_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY5_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY6_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XY7_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_8XYE_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_9XY0_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_ANNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_BNNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_CXNN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_DXYN_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_EX9E_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_EXA1_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX07_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX0A_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX15_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX18_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX1E_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX29_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX33_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX55_test)
+{
+    FAIL() << "Not yet implemented";
+}
+
+TEST_F(Chip8CPUTest, opcode_FX65_test)
+{
+    FAIL() << "Not yet implemented";
 }
