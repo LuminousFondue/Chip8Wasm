0bae4301c6717b9f71581e957077fb9d959ba322
LuminousFondue <kseldom@proton.me>
2025-08-20 17:35:10 -0400

    Implemented Chip8InputData

---
+++ src/Chip8InputData.cpp
@@ -0,0 +1,28 @@
+#include "Chip8InputData.h"
+
+Chip8InputData::Chip8InputData()
+{
+    for (int i = 0; i < 16; ++i)
+    {
+        keyStates[i] = false;
+    }
+}
+
+Chip8InputData::~Chip8InputData() = default;
+
+void Chip8InputData::setKeyState(int key, bool pressed)
+{
+    if (key >= 0 && key < 16)
+    {
+        keyStates[key] = pressed;
+    }
+}
+
+bool Chip8InputData::isKeyPressed(int key) const
+{
+    if (key >= 0 && key < 16)
+    {
+        return keyStates[key];
+    }
+    return false;
+}
