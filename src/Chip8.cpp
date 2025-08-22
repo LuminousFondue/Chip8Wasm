59304bc0fca645cf7ef37ac088cb1e25dbe78e28
LuminousFondue <kseldom@proton.me>
2025-08-21 20:56:55 -0400

    Added chip8 rom loading and an sdl display to test

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -4,7 +4,9 @@
 
 #include <cstdio>
 
-Chip8::Chip8() : memory_(), graphics_(), cpu_(memory_, graphics_, input_, delayTimer_, soundTimer_)
+Chip8::Chip8()
+    : memory_(), graphics_(), cpu_(memory_, graphics_, input_, delayTimer_, soundTimer_),
+      isPaused_(false)
 {
     spdlog::debug("Chip8 Created");
 }
@@ -22,4 +24,33 @@ void Chip8::reset()
     delayTimer_.reset();
     soundTimer_.reset();
     spdlog::debug("Chip8 reset to initial state");
+}
+
+void Chip8::loadROM(const uint8_t* romData, size_t romSize)
+{
+    for (size_t i = 0; i < romSize && i < memory_.MEMORY_SIZE; ++i)
+    {
+        memory_.write(0x200 + i, romData[i]);
+    }
+    spdlog::debug("ROM loaded into memory");
+}
+
+void Chip8::cycle()
+{
+    if (!isPaused_)
+    {
+        cpu_.cycle();
+        delayTimer_.update();
+        soundTimer_.update();
+    }
+}
+
+void Chip8::pause()
+{
+    isPaused_ = true;
+}
+
+void Chip8::resume()
+{
+    isPaused_ = false;
 }
