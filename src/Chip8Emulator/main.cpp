5c34366faf066c88210032ef97205d470e41082c
LuminousFondue <kseldom@proton.me>
2025-08-28 08:31:01 -0400

    Updated project to support different builds

--- src/Chip8Emulator/main.cpp
+++ src/Chip8Emulator/main.cpp
@@ -11,6 +11,13 @@
 #include "Chip8Emulator/Chip8Input.h"
 #include "Chip8Emulator/Chip8ROMLoader.h"
 
+chip8core::Chip8 chip8;
+Chip8Display     display(64, 32, 10);
+Chip8Input       input;
+Chip8Audio       audio;
+bool             running        = true;
+const int        cyclesPerFrame = 10;
+
 int main()
 {
     if (SDL_Init(SDL_INIT_AUDIO) < 0)
@@ -19,21 +26,24 @@ int main()
     }
     spdlog::set_level(spdlog::level::debug);
     spdlog::info("Application Started");
-    chip8core::Chip8             chip8;
-    const chip8core::Chip8Timer& soundTimer = chip8.getSoundTimer();
+
     Chip8ROMLoader::loadROM("../../../roms/6-keypad.ch8", chip8);
-    Chip8Display display(64, 32, 10);
-    Chip8Input   input;
-    Chip8Audio   audio;
-    bool         running        = true;
-    const int    cyclesPerFrame = 10;
 
     while (running)
     {
+        // Poll for input
         input.pollEvents(chip8.getInput(), running);
+
+        // Cycle Chip8
         chip8.cycle();
+
+        // Render display
         display.render(chip8.getGraphics());
-        audio.processAudio(soundTimer);
+
+        // Play audio
+        audio.processAudio(chip8.getSoundTimer());
+
+        // Delay SDL
         SDL_Delay(1);
     }
     spdlog::info("Application Ended");
